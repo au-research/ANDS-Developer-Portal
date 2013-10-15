331523f5de46f4080fef1970bbc9b80249c263fc
Minh Duc Nguyen <dekarvn@gmail.com>
2013-10-15 11:08:01 +1100

    added custom advanced search for registry widget doco

--- documentation/assets/js/registry_widget_doc.js
+++ documentation/assets/js/registry_widget_doc.js
@@ -1,5 +1,10 @@
 $(function(){
 	prettyPrint();
-	// $('#single_display').registry_widget({mode:'display_single'});
-	// $('#search_display').registry_widget({mode:'display_result'});
+	$('#advanced_search_example').registry_widget({
+		mode: 'search',
+		lookup:false,
+		auto_search:true,
+		search_btn_text: '<i class="icon icon-search"></i> Search',
+		result_template: '<div id="accordion" class="panel-group"> {{#docs}} <div class="panel panel-default"> <div class="panel-heading"> <h4 class="panel-title"> <a href="#{{slug}}" data-parent="#accordion" data-toggle="collapse" class="accordion-toggle collapsed"> {{list_title}} </a> </h4> </div> <div class="panel-collapse collapse" id="{{slug}}"> <div class="panel-body"> {{description}} </div> </div> </div> {{/docs}} </div>', 
+	});
 });
--- documentation/views/registry_widget.php
+++ documentation/views/registry_widget.php
@@ -21,21 +21,18 @@
 
 			<div id="k-main" class="clearfix col-lg-8 col-md-8 col-sm-12"><!-- starts main content -->
 				<article><!-- starts article short -->
-					<div class="alert alert-info">
-						<b>Developer Zone</b>
-						<p>Some basic web development knowledge may be needed to implement this widget</p>
-					</div>
+					
 
 					<h2 class="widget-title">Search Result Display Example</h2>
 					<ul class="nav nav-tabs">
-						<li class="active"><a href="#orcid-3-result" data-toggle="tab">Result</a></li>
-						<li><a href="#orcid-3-html" data-toggle="tab">HTML</a></li>
+						<li class="active"><a href="#registry_w-3-result" data-toggle="tab">Result</a></li>
+						<li><a href="#registry_w-3-html" data-toggle="tab">HTML</a></li>
 					</ul>
 					<div class="tab-content">
-						<div id="orcid-3-result" class="tab-pane fade active in">
+						<div id="registry_w-3-result" class="tab-pane fade active in">
 							<div id="search_display" data-query="q=fulltext:fish&rows=5" data-mode="display_result" class="registry_widget"></div>
 						</div>
-						<div id="orcid-3-html" class="tab-pane fade">
+						<div id="registry_w-3-html" class="tab-pane fade">
 							<pre class="prettyprint">
 &lt;div id="search_display" data-query="q=fulltext:fish&rows=5" data-mode="display_result" class="registry_widget"&gt;&lt;/div&gt;
 							</pre>
@@ -44,14 +41,14 @@
 
 					<h2 class="widget-title">Single Display Example</h2>
 					<ul class="nav nav-tabs">
-						<li class="active"><a href="#orcid-2-result" data-toggle="tab">Result</a></li>
-						<li><a href="#orcid-2-html" data-toggle="tab">HTML</a></li>
+						<li class="active"><a href="#registry_w-2-result" data-toggle="tab">Result</a></li>
+						<li><a href="#registry_w-2-html" data-toggle="tab">HTML</a></li>
 					</ul>
 					<div class="tab-content">
-						<div id="orcid-2-result" class="tab-pane fade active in">
+						<div id="registry_w-2-result" class="tab-pane fade active in">
 							<div id="single_display" data-query="AODN:93f4e867-0bac-45fa-acca-2881680627f7" data-mode="display_single" class="registry_widget"></div>
 						</div>
-						<div id="orcid-2-html" class="tab-pane fade">
+						<div id="registry_w-2-html" class="tab-pane fade">
 							<pre class="prettyprint">
 &lt;div id="single_display" data-query="AODN:93f4e867-0bac-45fa-acca-2881680627f7" data-mode="display_single" class="registry_widget"&gt;&lt;/div&gt;
 							</pre>
@@ -60,38 +57,78 @@
 				
 					<h2 class="widget-title">Simple Search Example</h2>
 					<ul class="nav nav-tabs">
-						<li class="active"><a href="#orcid-1-result" data-toggle="tab">Result</a></li>
-						<li><a href="#orcid-1-html" data-toggle="tab">HTML</a></li>
+						<li class="active"><a href="#registry_w-1-result" data-toggle="tab">Result</a></li>
+						<li><a href="#registry_w-1-html" data-toggle="tab">HTML</a></li>
 					</ul>
 					<div class="tab-content">
-						<div id="orcid-1-result" class="tab-pane fade active in">
+						<div id="registry_w-1-result" class="tab-pane fade active in">
 							<input type="text" class="registry_widget" value="fish">
 						</div>
-						<div id="orcid-1-html" class="tab-pane fade">
+						<div id="registry_w-1-html" class="tab-pane fade">
 							<pre class="prettyprint">
 &lt;input type="text" class="registry_widget" value="fish"&gt;
 							</pre>
 						</div>
 					</div>
 
+					<h2 class="widget-title">Advanced Search Example</h2>
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#registry_w-4-result" data-toggle="tab">Result</a></li>
+						<li><a href="#registry_w-4-html" data-toggle="tab">HTML</a></li>
+						<li><a href="#registry_w-4-js" data-toggle="tab">JS</a></li>
+					</ul>
+					<div class="tab-content">
+						<div id="registry_w-4-result" class="tab-pane fade active in">
+							<input type="text" value="fish" id="advanced_search_example">
+						</div>
+						<div id="registry_w-4-html" class="tab-pane fade">
+							<pre class="prettyprint">
+&lt;input type="text" class="registry_widget" value="fish"&gt;
+							</pre>
+						</div>
+						<div id="registry_w-4-js" class="tab-pane fade">
+							<pre class="prettyprint">
+$('#advanced_search_example').registry_widget({
+	mode: 'search',
+	lookup:false,
+	auto_search:true,
+	search_btn_text: '&lt;i class="icon icon-search">&lt;/i&gt; Search',
+	result_template: '&lt;div id="accordion" class="panel-group"&gt; {{#docs}} &lt;div class="panel panel-default"&gt; &lt;div class="panel-heading"&gt; &lt;h4 class="panel-title"&gt; &lt;a href="#{{slug}}" data-parent="#accordion" data-toggle="collapse" class="accordion-toggle collapsed"&gt; {{list_title}} &lt;/a&gt; &lt;/h4&gt; &lt;/div&gt; &lt;div class="panel-collapse collapse" id="{{slug}}"&gt; &lt;div class="panel-body"&gt; {{description}} &lt;/div&gt; &lt;/div&gt; &lt;/div&gt; {{/docs}} &lt;/div&gt;', 
+});
+							</pre>
+						</div>
+					</div>
+
 	
-					<h2 class="widget-title">License</h2>
-					<p>Apache License, Version 2.0: <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a></p>
+					
 				</article><!-- ends article short -->
 			</div><!-- ends main content -->
 			
 			<aside id="k-sidebar" class="col-lg-3 col-md-4 col-sm-12 col-lg-offset-1"><!-- starts sidebar -->
 				<div id="k-sidebar-splitter" class="clearfix section-space60"><span></span></div>
 				<ul id="k-sidebar-list" class="list-unstyled">
+
+					<li class="widget clearfix">
+						<div class="alert alert-info">
+							<b>Developer Zone</b>
+							<p>Some basic web development knowledge may be needed to implement this widget</p>
+						</div>
+					</li>
 					
-					<li class="widget widget_categories clearfix sticky">
-						<h2 class="widget-title">Demo and Download<span class="k-widget-title-tit"></span></h2>
+					<div class="sticky">
+					<li class="widget widget_categories clearfix">
+						<h2 class="widget-title">Download<span class="k-widget-title-tit"></span></h2>
 						<ul class="list-unstyled">
-							<li class="cat-item"><?php echo anchor(apps_url('registry_widget/download/'), '<i class="icon-white icon-download"></i> Demo', array('class'=>'btn btn-large btn-success')) ?></li>
 							<li class="cat-item"><?php echo anchor(apps_url('registry_widget/download/'), '<i class="icon-white icon-download"></i> Download minified', array('class'=>'btn btn-large btn-success')) ?></li>
 							<li class="cat-item"><?php echo anchor(apps_url('registry_widget/download/'), '<i class="icon-white icon-download"></i> Download uncompressed', array('class'=>'btn btn-large btn-success')) ?></li>
 						</ul>
 					</li>
+
+					<li class="widget clearfix">
+						<h2 class="widget-title">License<span class="k-widget-title-tit"></span></h2>
+						<p>Apache License, Version 2.0: <a href="http://www.apache.org/licenses/LICENSE-2.0">http://www.apache.org/licenses/LICENSE-2.0</a></p>
+					</li>
+					</div>
 				
 				</ul>
 			</aside><!-- ends sidebar -->
