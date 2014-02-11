b8e21e8630c6f92c4879e4082bbf56d0c0fb9ae9
Elizabeth Woods <u3485947@devl.ands.org.au>
2014-02-12 10:01:17 +1100

    added announcement about changes to location widget due to gazeteer being offline

--- documentation/views/widgets/location_widget.php
+++ documentation/views/widgets/location_widget.php
@@ -82,7 +82,14 @@ $("#mapContainer").ands_location_widget({
 							<b>Developer Zone</b>
 							<p>Some basic web development knowledge may be needed to implement this widget</p>
 						</div>
-					</li>
+				
+						<div class="alert alert-warning">
+							<b>Announcement</b>
+							<p>The Australian Gazetteer service which is integrated into the Location Capture widget is currently offline. 
+As an interim solution the Australian Gazetteer integration has been removed from the latest version of the widget.</p>
+<p><a href="http://community.ands.org.au/viewtopic.php?f=211&t=3301&sid=979668495e440692b6cf95d33dacf567" target="_blank">More info </a></p>
+						</div>
+					</li>					
 					<div class="sticky">
 					<li class="widget widget_categories clearfix">
 						<h2 class="widget-title">Software Download<span class="k-widget-title-tit"></span></h2>
